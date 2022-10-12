cad0836850939c955d46005bc15abda7c8e88f5e
sydvys <aysuseyidova36@gmail.com>
2022-10-12 15:13:33 +0400

    .

---
+++ js-aysu_fe06/1th lesson/task 2/hlp.js
@@ -0,0 +1,6 @@
+var a = prompt('enter birth year');
+var b = prompt('enter current year');
+
+age = b-a;
+
+console.log('you are ' + age + ' years old' )  ;
---
+++ js-aysu_fe06/1th lesson/task 3/hlp.js
@@ -0,0 +1,5 @@
+var km = prompt('enter the number of km you want to convert');
+
+answer = km * 0.621;
+
+console.log(km + ' kilometers equals ' + answer + ' miles');
