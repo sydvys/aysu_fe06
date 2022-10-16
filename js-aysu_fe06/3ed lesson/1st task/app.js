235e7764122f8b06ce3319e3b5d6a2924113d4b7
sydvys <aysuseyidova36@gmail.com>
2022-10-16 13:41:58 +0400

    .

---
+++ js-aysu_fe06/3ed lesson/1st task/app.js
@@ -0,0 +1,9 @@
+let num = Number(prompt('pick a number between 1 - 50'));
+
+if(num == 29){
+    console.log('you are correct');
+}else if(num > 50){
+    console.log('the number you picked is over 50');
+}else{
+    console.log('incorrect, please try again');
+}
