983186afb08641caa6620bd3c79e310af6851841
sydvys <aysuseyidova36@gmail.com>
2022-11-08 17:59:13 +0400

    .

---
+++ js-aysu_fe06/11th lesson/main.js
@@ -0,0 +1,30 @@
+const text = document.querySelectorAll('button')
+
+text[0].style.backgroundColor = 'gray'
+text[0].onclick = () => {
+    document.body.style.backgroundColor = 'gray'
+}
+
+text[1].style.backgroundColor = 'red'
+text[1].onclick = () => {
+    document.body.style.backgroundColor = 'red'
+}
+
+text[2].style.backgroundColor = 'blue'
+text[2].onclick = () => {
+    document.body.style.backgroundColor = 'blue'
+}
+
+text[3].style.backgroundColor = 'yellow'
+text[3].onclick = () => {
+    document.body.style.backgroundColor = 'yellow'
+}
+
+
+
+
+
+
+
+
+
